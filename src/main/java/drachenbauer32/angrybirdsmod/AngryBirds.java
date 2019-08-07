75d669673c6ed98c442faae3f4a40c73c7d5fa86
Drachenbauer <47259059+Drachenbauer@users.noreply.github.com>
2019-08-08 00:35:12 +0200

    sorted creativetab-items

--- src/main/java/drachenbauer32/angrybirdsmod/AngryBirds.java
+++ src/main/java/drachenbauer32/angrybirdsmod/AngryBirds.java
@@ -1,10 +1,6 @@
 package drachenbauer32.angrybirdsmod;
 
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
-
-//import com.google.common.collect.Ordering;
 
 import drachenbauer32.angrybirdsmod.blocks.BalloonBlock;
 import drachenbauer32.angrybirdsmod.blocks.BalloonBlockTop;
@@ -56,7 +52,6 @@ import net.minecraft.item.SpawnEggItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 //import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.common.MinecraftForge;
@@ -70,7 +65,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(Reference.MOD_ID)
 public class AngryBirds
 {   
-    public static Comparator<? super ItemStack> itemSorter;
+    public static Comparator<? super Item> itemSorter;
     
     public AngryBirds() 
     {
@@ -81,65 +76,7 @@ public class AngryBirds
     
     void preInit()
     {
-        @SuppressWarnings("unused")
-        List<Item> order = Arrays.asList(AngryBirdsItems.red_egg,
-                                         AngryBirdsItems.chuck_egg,
-                                         AngryBirdsItems.blues_egg,
-                                         AngryBirdsItems.bomb_egg,
-                                         AngryBirdsItems.mathilda_egg,
-                                         AngryBirdsItems.terence_egg,
-                                         AngryBirdsItems.silver_egg,
-                                         AngryBirdsItems.bubbles_egg,
-                                         AngryBirdsItems.hal_egg,
-                                         AngryBirdsItems.stella_egg,
-                                         AngryBirdsItems.poppy_egg,
-                                         AngryBirdsItems.willow_egg,
-                                         AngryBirdsItems.dahlia_egg,
-                                         AngryBirdsItems.luca_egg,
-                                         AngryBirdsItems.ice_bird_egg,
-                                         
-                                         AngryBirdsItems.slingshot,
-                                         AngryBirdsItems.red_shot,
-                                         AngryBirdsItems.chuck_shot,
-                                         AngryBirdsItems.blues_shot,
-                                         AngryBirdsItems.bomb_shot,
-                                         AngryBirdsItems.mathilda_shot,
-                                         AngryBirdsItems.terence_shot,
-                                         AngryBirdsItems.silver_shot,
-                                         AngryBirdsItems.bubbles_shot,
-                                         AngryBirdsItems.hal_shot,
-                                         AngryBirdsItems.stella_shot,
-                                         AngryBirdsItems.poppy_shot,
-                                         AngryBirdsItems.willow_shot,
-                                         AngryBirdsItems.dahlia_shot,
-                                         AngryBirdsItems.luca_shot,
-                                         AngryBirdsItems.ice_bird_shot,
-                                           
-                                         AngryBirdsItems.balloon_block,
-                                         AngryBirdsItems.egg_block,
-                                         AngryBirdsItems.nest_block,
-                                         AngryBirdsItems.slingshot_acacia_block,
-                                         AngryBirdsItems.slingshot_acacia_2_block,
-                                         AngryBirdsItems.slingshot_birch_block,
-                                         AngryBirdsItems.slingshot_birch_2_block,
-                                         AngryBirdsItems.slingshot_dark_oak_block,
-                                         AngryBirdsItems.slingshot_dark_oak_2_block,
-                                         AngryBirdsItems.slingshot_jungle_block,
-                                         AngryBirdsItems.slingshot_jungle_2_block,
-                                         AngryBirdsItems.slingshot_oak_block,
-                                         AngryBirdsItems.slingshot_oak_2_block,
-                                         AngryBirdsItems.slingshot_spruce_block,
-                                         AngryBirdsItems.slingshot_spruce_2_block,
-                                         AngryBirdsItems.acacia_planks_frame_block,
-                                         AngryBirdsItems.birch_planks_frame_block,
-                                         AngryBirdsItems.dark_oak_planks_frame_block,
-                                         AngryBirdsItems.jungle_planks_frame_block,
-                                         AngryBirdsItems.oak_planks_frame_block,
-                                         AngryBirdsItems.spruce_planks_frame_block,
-                                         AngryBirdsItems.stone_frame_block,
-                                         AngryBirdsItems.blue_ice_frame_block);
         
-        //itemSorter = Ordering.explicit(order).onResultOf(ItemStack::g﻿etIt﻿em);
     }
     
     private void setup(final FMLCommonSetupEvent event)
--- src/main/java/drachenbauer32/angrybirdsmod/util/AngryBirdsItemGroup.java
+++ src/main/java/drachenbauer32/angrybirdsmod/util/AngryBirdsItemGroup.java
@@ -1,7 +1,10 @@
 package drachenbauer32.angrybirdsmod.util;
 
-//import drachenbauer32.angrybirdsmod.AngryBirds;
+import java.util.Arrays;
+import java.util.List;
+
 import drachenbauer32.angrybirdsmod.init.AngryBirdsItems;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
@@ -20,9 +23,70 @@ public class AngryBirdsItemGroup extends ItemGroup
     }
     
     @Override
-    public void fill(NonNullList<ItemStack> items)
+    public void fill(NonNullList<ItemStack> itemStacks)
     {
-        super.fill(items);
-        //items.sort(AngryBirds.tabSorter);
+        List<Item> items = Arrays.asList(AngryBirdsItems.red_egg,
+                                         AngryBirdsItems.chuck_egg,
+                                         AngryBirdsItems.blues_egg,
+                                         AngryBirdsItems.bomb_egg,
+                                         AngryBirdsItems.mathilda_egg,
+                                         AngryBirdsItems.terence_egg,
+                                         AngryBirdsItems.silver_egg,
+                                         AngryBirdsItems.bubbles_egg,
+                                         AngryBirdsItems.hal_egg,
+                                         AngryBirdsItems.stella_egg,
+                                         AngryBirdsItems.poppy_egg,
+                                         AngryBirdsItems.willow_egg,
+                                         AngryBirdsItems.dahlia_egg,
+                                         AngryBirdsItems.luca_egg,
+                                         AngryBirdsItems.ice_bird_egg,
+                                         
+                                         AngryBirdsItems.slingshot,
+                                         AngryBirdsItems.red_shot,
+                                         AngryBirdsItems.chuck_shot,
+                                         AngryBirdsItems.blues_shot,
+                                         AngryBirdsItems.bomb_shot,
+                                         AngryBirdsItems.mathilda_shot,
+                                         AngryBirdsItems.terence_shot,
+                                         AngryBirdsItems.silver_shot,
+                                         AngryBirdsItems.bubbles_shot,
+                                         AngryBirdsItems.hal_shot,
+                                         AngryBirdsItems.stella_shot,
+                                         AngryBirdsItems.poppy_shot,
+                                         AngryBirdsItems.willow_shot,
+                                         AngryBirdsItems.dahlia_shot,
+                                         AngryBirdsItems.luca_shot,
+                                         AngryBirdsItems.ice_bird_shot,
+                                           
+                                         AngryBirdsItems.balloon_block,
+                                         AngryBirdsItems.egg_block,
+                                         AngryBirdsItems.nest_block,
+                                         AngryBirdsItems.slingshot_acacia_block,
+                                         AngryBirdsItems.slingshot_acacia_2_block,
+                                         AngryBirdsItems.slingshot_birch_block,
+                                         AngryBirdsItems.slingshot_birch_2_block,
+                                         AngryBirdsItems.slingshot_dark_oak_block,
+                                         AngryBirdsItems.slingshot_dark_oak_2_block,
+                                         AngryBirdsItems.slingshot_jungle_block,
+                                         AngryBirdsItems.slingshot_jungle_2_block,
+                                         AngryBirdsItems.slingshot_oak_block,
+                                         AngryBirdsItems.slingshot_oak_2_block,
+                                         AngryBirdsItems.slingshot_spruce_block,
+                                         AngryBirdsItems.slingshot_spruce_2_block,
+                                         AngryBirdsItems.acacia_planks_frame_block,
+                                         AngryBirdsItems.birch_planks_frame_block,
+                                         AngryBirdsItems.dark_oak_planks_frame_block,
+                                         AngryBirdsItems.jungle_planks_frame_block,
+                                         AngryBirdsItems.oak_planks_frame_block,
+                                         AngryBirdsItems.spruce_planks_frame_block,
+                                         AngryBirdsItems.stone_frame_block,
+                                         AngryBirdsItems.blue_ice_frame_block);
+        
+        itemStacks.clear();
+        
+        for (Item item : items)
+        {
+            itemStacks.add(new ItemStack(item));
+        }
     }
 }
