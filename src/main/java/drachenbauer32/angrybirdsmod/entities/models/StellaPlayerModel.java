5a21c026c7dd843cbcc64341c1d74aa188f07aa9
Drachenbauer <47259059+Drachenbauer@users.noreply.github.com>
2019-08-11 22:54:20 +0200

    Update StellaPlayerModel.java

--- src/main/java/drachenbauer32/angrybirdsmod/entities/models/StellaPlayerModel.java
+++ src/main/java/drachenbauer32/angrybirdsmod/entities/models/StellaPlayerModel.java
@@ -4,6 +4,11 @@ import drachenbauer32.angrybirdsmod.entities.StellaPlayerEntity;
 import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.client.renderer.entity.model.RendererModel;
 import net.minecraft.client.renderer.model.ModelBox;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.Hand;
+import net.minecraft.util.HandSide;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -17,6 +22,10 @@ public class StellaPlayerModel extends EntityModel<StellaPlayerEntity>
     private final RendererModel bone5;
     private final RendererModel bipedRightArm;
     private final RendererModel bipedLeftArm;
+    public StellaPlayerModel.ArmPose rightArmPose = StellaPlayerModel.ArmPose.EMPTY;
+    public StellaPlayerModel.ArmPose leftArmPose = StellaPlayerModel.ArmPose.EMPTY;
+    public boolean isSneak;
+    public float field_205061_a;
     
     public StellaPlayerModel()
     {
@@ -84,13 +93,268 @@ public class StellaPlayerModel extends EntityModel<StellaPlayerEntity>
     public void setRotationAngles(StellaPlayerEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw,
                                   float headPitch, float scaleFactor)
     {
-        bipedHead.rotateAngleX = headPitch * 0.017453292f;
-        bipedHead.rotateAngleY = netHeadYaw * 0.017453292f;
+        boolean flag = entity instanceof LivingEntity && ((LivingEntity)entity).getTicksElytraFlying() > 4;
+        boolean flag1 = entity.isSwimming();
+        this.bipedHead.rotateAngleY = netHeadYaw * ((float)Math.PI / 180F);
+        
+        if (flag)
+        {
+            this.bipedHead.rotateAngleX = (-(float)Math.PI / 4F);
+        }
+        else if (this.field_205061_a > 0.0F)
+        {
+            if (flag1)
+            {
+                this.bipedHead.rotateAngleX = this.func_205060_a(this.bipedHead.rotateAngleX, (-(float)Math.PI / 4F), this.field_205061_a);
+            }
+            else
+            {
+                this.bipedHead.rotateAngleX = this.func_205060_a(this.bipedHead.rotateAngleX, headPitch * ((float)Math.PI / 180F), this.field_205061_a);
+            }
+        }
+        else
+        {
+            this.bipedHead.rotateAngleX = headPitch * ((float)Math.PI / 180F);
+        }
+        
+        this.bipedRightArm.rotationPointZ = 0.0F;
+        this.bipedRightArm.rotationPointX = -5.0F;
+        this.bipedLeftArm.rotationPointZ = 0.0F;
+        this.bipedLeftArm.rotationPointX = 5.0F;
+        float f = 1.0F;
+        
+        if (flag)
+        {
+            f = (float)entity.getMotion().lengthSquared();
+            f = f / 0.2F;
+            f = f * f * f;
+        }
+        
+        if (f < 1.0F)
+        {
+            f = 1.0F;
+        }
+        
+        this.bipedRightArm.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 2.0F * limbSwingAmount * 0.5F / f;
+        this.bipedLeftArm.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 2.0F * limbSwingAmount * 0.5F / f;
+        this.bipedRightArm.rotateAngleZ = 0.0F;
+        this.bipedLeftArm.rotateAngleZ = 0.0F;
+        
+        if (this.isSitting)
+        {
+            this.bipedRightArm.rotateAngleX += (-(float)Math.PI / 5F);
+            this.bipedLeftArm.rotateAngleX += (-(float)Math.PI / 5F);
+        }
+        
+        this.bipedRightArm.rotateAngleY = 0.0F;
+        this.bipedRightArm.rotateAngleZ = 0.0F;
+        
+        switch(this.leftArmPose)
+        {
+            case EMPTY:
+            this.bipedLeftArm.rotateAngleY = 0.0F;
+            break;
+            
+            case BLOCK:
+            this.bipedLeftArm.rotateAngleX = this.bipedLeftArm.rotateAngleX * 0.5F - 0.9424779F;
+            this.bipedLeftArm.rotateAngleY = ((float)Math.PI / 6F);
+            break;
+            
+            case ITEM:
+            this.bipedLeftArm.rotateAngleX = this.bipedLeftArm.rotateAngleX * 0.5F - ((float)Math.PI / 10F);
+            this.bipedLeftArm.rotateAngleY = 0.0F;
+            break;
+            
+            case THROW_SPEAR:
+            break;
+            
+            case BOW_AND_ARROW:
+        }
+        
+        switch(this.rightArmPose)
+        {
+            case EMPTY:
+            this.bipedRightArm.rotateAngleY = 0.0F;
+            break;
+            
+            case BLOCK:
+            this.bipedRightArm.rotateAngleX = this.bipedRightArm.rotateAngleX * 0.5F - 0.9424779F;
+            this.bipedRightArm.rotateAngleY = (-(float)Math.PI / 6F);
+            break;
+            
+            case ITEM:
+            this.bipedRightArm.rotateAngleX = this.bipedRightArm.rotateAngleX * 0.5F - ((float)Math.PI / 10F);
+            this.bipedRightArm.rotateAngleY = 0.0F;
+            break;
+            
+            case THROW_SPEAR:
+            this.bipedRightArm.rotateAngleX = this.bipedRightArm.rotateAngleX * 0.5F - (float)Math.PI;
+            this.bipedRightArm.rotateAngleY = 0.0F;
+            break;
+            
+            case BOW_AND_ARROW:
+        }
+        
+        if (this.leftArmPose == StellaPlayerModel.ArmPose.THROW_SPEAR && this.rightArmPose != StellaPlayerModel.ArmPose.BLOCK && this.rightArmPose != StellaPlayerModel.ArmPose.THROW_SPEAR && this.rightArmPose != StellaPlayerModel.ArmPose.BOW_AND_ARROW)
+        {
+            this.bipedLeftArm.rotateAngleX = this.bipedLeftArm.rotateAngleX * 0.5F - (float)Math.PI;
+            this.bipedLeftArm.rotateAngleY = 0.0F;
+        }
+        
+        if (this.isSneak)
+        {
+            this.bipedRightArm.rotateAngleX += 0.4F;
+            this.bipedLeftArm.rotateAngleX += 0.4F;
+            this.bipedHead.rotationPointY = 1.0F;
+        }
+        else
+        {
+            this.bipedHead.rotationPointY = 0.0F;
+        }
+        
+        this.bipedRightArm.rotateAngleZ += MathHelper.cos(ageInTicks * 0.09F) * 0.05F + 0.05F;
+        this.bipedLeftArm.rotateAngleZ -= MathHelper.cos(ageInTicks * 0.09F) * 0.05F + 0.05F;
+        this.bipedRightArm.rotateAngleX += MathHelper.sin(ageInTicks * 0.067F) * 0.05F;
+        this.bipedLeftArm.rotateAngleX -= MathHelper.sin(ageInTicks * 0.067F) * 0.05F;
+        
+        if (this.rightArmPose == StellaPlayerModel.ArmPose.BOW_AND_ARROW)
+        {
+            this.bipedRightArm.rotateAngleY = -0.1F + this.bipedHead.rotateAngleY;
+            this.bipedLeftArm.rotateAngleY = 0.1F + this.bipedHead.rotateAngleY + 0.4F;
+            this.bipedRightArm.rotateAngleX = (-(float)Math.PI / 2F) + this.bipedHead.rotateAngleX;
+            this.bipedLeftArm.rotateAngleX = (-(float)Math.PI / 2F) + this.bipedHead.rotateAngleX;
+        }
+        else if (this.leftArmPose == StellaPlayerModel.ArmPose.BOW_AND_ARROW && this.rightArmPose != StellaPlayerModel.ArmPose.THROW_SPEAR && this.rightArmPose != StellaPlayerModel.ArmPose.BLOCK)
+        {
+            this.bipedRightArm.rotateAngleY = -0.1F + this.bipedHead.rotateAngleY - 0.4F;
+            this.bipedLeftArm.rotateAngleY = 0.1F + this.bipedHead.rotateAngleY;
+            this.bipedRightArm.rotateAngleX = (-(float)Math.PI / 2F) + this.bipedHead.rotateAngleX;
+            this.bipedLeftArm.rotateAngleX = (-(float)Math.PI / 2F) + this.bipedHead.rotateAngleX;
+        }
+        
+        if (this.field_205061_a > 0.0F)
+        {
+            float f4 = limbSwing % 26.0F;
+            float f5 = this.swingProgress > 0.0F ? 0.0F : this.field_205061_a;
+            
+            if (f4 < 14.0F)
+            {
+                this.bipedLeftArm.rotateAngleX = this.func_205060_a(this.bipedLeftArm.rotateAngleX, 0.0F, this.field_205061_a);
+                this.bipedRightArm.rotateAngleX = this.func_205059_b(this.bipedRightArm.rotateAngleX, 0.0F, f5);
+                this.bipedLeftArm.rotateAngleY = this.func_205060_a(this.bipedLeftArm.rotateAngleY, (float)Math.PI, this.field_205061_a);
+                this.bipedRightArm.rotateAngleY = this.func_205059_b(this.bipedRightArm.rotateAngleY, (float)Math.PI, f5);
+                this.bipedLeftArm.rotateAngleZ = this.func_205060_a(this.bipedLeftArm.rotateAngleZ, (float)Math.PI + 1.8707964F * this.func_203068_a(f4) / this.func_203068_a(14.0F), this.field_205061_a);
+                this.bipedRightArm.rotateAngleZ = this.func_205059_b(this.bipedRightArm.rotateAngleZ, (float)Math.PI - 1.8707964F * this.func_203068_a(f4) / this.func_203068_a(14.0F), f5);
+            }
+            else if (f4 >= 14.0F && f4 < 22.0F)
+            {
+                float f7 = (f4 - 14.0F) / 8.0F;
+                this.bipedLeftArm.rotateAngleX = this.func_205060_a(this.bipedLeftArm.rotateAngleX, ((float)Math.PI / 2F) * f7, this.field_205061_a);
+                this.bipedRightArm.rotateAngleX = this.func_205059_b(this.bipedRightArm.rotateAngleX, ((float)Math.PI / 2F) * f7, f5);
+                this.bipedLeftArm.rotateAngleY = this.func_205060_a(this.bipedLeftArm.rotateAngleY, (float)Math.PI, this.field_205061_a);
+                this.bipedRightArm.rotateAngleY = this.func_205059_b(this.bipedRightArm.rotateAngleY, (float)Math.PI, f5);
+                this.bipedLeftArm.rotateAngleZ = this.func_205060_a(this.bipedLeftArm.rotateAngleZ, 5.012389F - 1.8707964F * f7, this.field_205061_a);
+                this.bipedRightArm.rotateAngleZ = this.func_205059_b(this.bipedRightArm.rotateAngleZ, 1.2707963F + 1.8707964F * f7, f5);
+            }
+            else if (f4 >= 22.0F && f4 < 26.0F)
+            {
+                float f6 = (f4 - 22.0F) / 4.0F;
+                this.bipedLeftArm.rotateAngleX = this.func_205060_a(this.bipedLeftArm.rotateAngleX, ((float)Math.PI / 2F) - ((float)Math.PI / 2F) * f6, this.field_205061_a);
+                this.bipedRightArm.rotateAngleX = this.func_205059_b(this.bipedRightArm.rotateAngleX, ((float)Math.PI / 2F) - ((float)Math.PI / 2F) * f6, f5);
+                this.bipedLeftArm.rotateAngleY = this.func_205060_a(this.bipedLeftArm.rotateAngleY, (float)Math.PI, this.field_205061_a);
+                this.bipedRightArm.rotateAngleY = this.func_205059_b(this.bipedRightArm.rotateAngleY, (float)Math.PI, f5);
+                this.bipedLeftArm.rotateAngleZ = this.func_205060_a(this.bipedLeftArm.rotateAngleZ, (float)Math.PI, this.field_205061_a);
+                this.bipedRightArm.rotateAngleZ = this.func_205059_b(this.bipedRightArm.rotateAngleZ, (float)Math.PI, f5);
+            }
+            
+            //float f8 = 0.3F;
+            //float f9 = 0.33333334F;
+        }
     }
     
-    @Override
-    public void setLivingAnimations(StellaPlayerEntity entity, float p_212843_2_, float p_212843_3_, float p_212843_4_)
+    public void setLivingAnimations(StellaPlayerEntity entity, float limbSwing, float limbSwingAmount, float partialTickTime) 
     {
-        super.setLivingAnimations(entity, p_212843_2_, p_212843_3_, p_212843_4_);
+        this.field_205061_a = entity.getSwimAnimation(partialTickTime);
+        super.setLivingAnimations(entity, limbSwing, limbSwingAmount, partialTickTime);
+    }
+    
+    protected float func_205060_a(float p_205060_1_, float p_205060_2_, float p_205060_3_)
+    {
+        float f;
+        for(f = p_205060_2_ - p_205060_1_; f < -(float)Math.PI; f += ((float)Math.PI * 2F))
+        {
+            ;
+        }
+        
+        while(f >= (float)Math.PI)
+        {
+            f -= ((float)Math.PI * 2F);
+        }
+        
+        return p_205060_1_ + p_205060_3_ * f;
+    }
+    
+    private float func_205059_b(float p_205059_1_, float p_205059_2_, float p_205059_3_)
+    {
+        return p_205059_1_ + (p_205059_2_ - p_205059_1_) * p_205059_3_;
+    }
+    
+    private float func_203068_a(float p_203068_1_)
+    {
+        return -65.0F * p_203068_1_ + p_203068_1_ * p_203068_1_;
+    }
+    
+    public void setModelAttributes(EntityModel<StellaPlayerEntity> model)
+    {
+        super.setModelAttributes(model);
+        
+        if (model instanceof StellaPlayerModel)
+        {
+            StellaPlayerModel modelbiped = (StellaPlayerModel)model;
+            this.leftArmPose = modelbiped.leftArmPose;
+            this.rightArmPose = modelbiped.rightArmPose;
+            this.isSneak = modelbiped.isSneak;
+        }
+    }
+    
+    public void setVisible(boolean visible)
+    {
+        this.bipedHead.showModel = visible;
+        this.bipedRightArm.showModel = visible;
+        this.bipedLeftArm.showModel = visible;
+    }
+    
+    public void postRenderArm(float scale, HandSide side)
+    {
+        this.getArmForSide(side).postRender(scale);
+    }
+    
+    protected RendererModel getArmForSide(HandSide side)
+    {
+        return side == HandSide.LEFT ? this.bipedLeftArm : this.bipedRightArm;
+    }
+    
+    protected HandSide getMainHand(Entity entityIn)
+    {
+        if (entityIn instanceof LivingEntity)
+        {
+            LivingEntity entitylivingbase = (LivingEntity)entityIn;
+            HandSide enumhandside = entitylivingbase.getPrimaryHand();
+            return entitylivingbase.swingingHand == Hand.MAIN_HAND ? enumhandside : enumhandside.opposite();
+        }
+        else
+        {
+            return HandSide.RIGHT;
+        }
+    }
+    
+    @OnlyIn(Dist.CLIENT)
+    public static enum ArmPose
+    {
+        EMPTY,
+        ITEM,
+        BLOCK,
+        BOW_AND_ARROW,
+        THROW_SPEAR;
     }
 }
